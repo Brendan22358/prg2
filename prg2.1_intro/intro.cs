2d9fc5236530e5e07a1c51c551b239ec1606a0b2
Brendan Gallagher <brendangallagher@Brendans-MacBook-Pro.local>
2018-02-01 08:34:52 -0600

    lol

--- prg2.1_intro/intro.cs
+++ prg2.1_intro/intro.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+//For Lists you need this line
+
 
 namespace prg2._1_intro
 {
@@ -7,29 +10,75 @@ namespace prg2._1_intro
         public Intro(string[] args)
         {
             Console.WriteLine("Launched intro");
+
+            List<string> students = new List<string>();
+
+            students.Add("Slosar");
+            students.Add("Engel");
+            students.Add("Yoon");
+            students.Add("Gallagher");
+
+            foreach(string student in students)
+            {
+                Console.Write(student + " ");
+            }
+           
+            Console.WriteLine();
+
+            while(students.Length != 10)
+            {
+                students.Add("empty spot");
+            }
+
+            /*
+            
             string answer= Console.ReadLine();
+            
             Console.WriteLine("Enter a number");
+            
             int x = Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("Enter another number!");
+            
             int y = Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("The sum of your two numbers is:");
+            
             int z = x + y;
+            
             Console.WriteLine(z);
+            
             double v = 5.0;
+            
             Console.WriteLine(v);
+            
             bool xIsEqualtoY = x == y;
+            
             if(x % 2 == 0)
+            
             {
+            
                 Console.WriteLine("Divisible by 2");
+            
             }   
+            
             else if(x % 3 == 0)
+            
             {
+            
                 Console.WriteLine("Divisible by 3");
+            
             }
+            
             else
+            
             {
+            
                 Console.WriteLine("Is not divisible");
+            
             }
+
+            */
 
         } 
     }
