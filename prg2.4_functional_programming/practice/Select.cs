43aea37831664017312353e8ed178afdf02f12f4
Brendan Gallagher <brendangallagher@Brendans-MacBook-Pro.local>
2018-03-20 08:08:35 -0500

    hi

--- prg2.4_functional_programming/practice/Select.cs
+++ prg2.4_functional_programming/practice/Select.cs
@@ -17,12 +17,38 @@ namespace prg2._4_functional_programming
             {
                 Console.Write(word + " ");
             }
-            List<string> changedWords = words.Select(word => word.ToLower() == "gallagher" ? "Gallagher is Awesome" : word).ToList();
+            List<string> changedWords = words.Select(word => word.ToLower() == "programming2" ? "Programming2 is Awesome" : word).ToList();
+            List<string> approvedWords = changedWords.Where(word => !word.ToLower().Contains("cheeto")).ToList();
+
+            List<string> cominedWords = words.Zip(changedWords,(word1,word2)=>word1+":"+word2).ToList();
+
             Console.WriteLine();
             foreach(string word in changedWords)
             {
                 Console.Write(word + " ");
             }
+            Console.WriteLine();
+            foreach(string word in approvedWords)
+            {
+                Console.Write(word + " ");
+            }
+
+            DoSomething(Something);
+            DoSomething(MakeUpperCase);
+
+        }
+        public string Something(string msg)
+        {
+            return "Help me Obi Won Kanobi!!!";
+        }
+        public void DoSomething(Func<string,string> anything)
+        {
+            Console.WriteLine(anything("please"));
+            anything(anything("okay"));
+        }
+        public string MakeUpperCase(string msg)
+        {
+            return msg.ToUpper();
         }
     }
 }
